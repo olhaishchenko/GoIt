6ca42c645c25df18bc4c22cbba7f82969d840d85
olhaishchenko <olga.ua.olga@gmail.com>
2022-11-02 10:39:23 +0100

    commit

---
+++ modul8/birthday/birthday.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta, date
+
+users = []
+with open('birthday.txt', 'r') as bh:
+    while True:
+        line = bh.readline()
+        if not line:
+            break
+        line_birthday = line.strip().split(' ')
+        user = {
+            'name': line_birthday[0],
+            'birthday': datetime.strptime(line_birthday[1], '%d.%m.%Y').date(),
+            }
+        users.append(user)
+    print(users)
+
+
+def interval_week():
+    day_today = datetime.today().date()
+    day = timedelta(days=1)
+    weeks = timedelta(weeks=1)
+    begin_interval = day_today + day
+    end_interval = day_today + weeks
+    print(begin_interval, end_interval)
+    return(begin_interval, end_interval)
+
+def get_birthdays_per_week():
+    (first_day, last_day) = interval_week()
+    begin_week = first_day.timetuple().tm_yday
+    end_week = last_day.timetuple().tm_yday
+    week_birthday = {}
+    for user in users:
+        user['birthday'] = user['birthday'].replace(year=2022)
+        if begin_week < user['birthday'].timetuple().tm_yday < end_week:
+            week_birthday[user['birthday'].strftime('%A')] = user['name']
+    print(week_birthday)
+
+
+    # print(first_day, last_day)
+
+get_birthdays_per_week()
+
