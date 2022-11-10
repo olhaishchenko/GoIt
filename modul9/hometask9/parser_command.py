58f218dfb747144e0496e8942ccb67bba0a41eee
olhaishchenko <olga.ua.olga@gmail.com>
2022-11-10 23:19:59 +0100

    commit changes

--- modul9/hometask9/parser_command.py
+++ modul9/hometask9/parser_command.py
@@ -1,5 +1,6 @@
-def hello(text):
-    return "How can I help you?"
+def hello():
+    text_output = "How can I help you?"
+    return print(text_output)
 
 
 kontakt_number = {}
@@ -9,7 +10,8 @@ def add_phone(name_phone: list):
     number_phone = name_phone[1]
     name = name_phone[0]
     kontakt_number[name] = number_phone
-    return "Number added"
+    text_output = "Number added"
+    return print(text_output)
 
 
 def change(name_phone: list):
@@ -18,22 +20,30 @@ def change(name_phone: list):
     l = kontakt_number[name]  # перевірка існування ім'я
     for char in filter(lambda value: value == name, kontakt_number.keys()):
         kontakt_number[name] = number_phone
-    return "Numer changed"
+    text_output = "Numer changed"
+    return print(text_output)
 
 
 def phone(text: str):
     name = text[0]
     l = kontakt_number[name]  # перевірка існування ім'я
-    return kontakt_number[name]
+    return print(kontakt_number[name])
 
 
-def show_all(text: str):
-    return kontakt_number
+def show_all():
+    return print(kontakt_number)
 
 
 def good_bye(text: str):
-    return "Good bye"
+    if text[0] == 'bye':
+        text_output = "Good bye"
+        return text_output
     exit(0)
+
+
+def bye():
+    text_output = "Good bye"
+    return text_output
 
 
 INPUT_HANDLER = {
@@ -43,8 +53,8 @@ INPUT_HANDLER = {
     "phone": phone,
     "show": show_all,
     "good": good_bye,
-    "close": good_bye,
-    "exit": good_bye
+    "close": bye,
+    "exit": bye
 }
 
 
@@ -55,7 +65,8 @@ def input_error(func):
         except IndexError:
             print("Give me name and phone please splitted by space")
         except KeyError:
-            print("Enter: add_name_phone, change_name_phone, phone_name, show all, good bye, close, exit to continue")
+            print("Enter: add (name phone), change (name phone), \
+phone (name), show all, good bye, close, exit to continue")
         except ValueError:
             print("Enter right user name")
     return wrapper
@@ -64,15 +75,16 @@ def input_error(func):
 @input_error
 def call_handler(text: str):
     list_text = text.split(' ')
-    if list_text[0] in INPUT_HANDLER:
+    if len(list_text) == 1:
+        INPUT_HANDLER[list_text[0]]()
+    else:
         INPUT_HANDLER[list_text[0]](list_text[1:])
 
 
 def main():
     while True:
         text = input("Hello, input command: ").lower()
-        print(call_handler(text))
-
+        call_handler(text)
 
 
 if __name__ == "__main__":
