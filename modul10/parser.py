868ab4278988002319b432d2a711453f75f238b5
olhaishchenko <olga.ua.olga@gmail.com>
2022-11-21 16:22:05 +0100

    commit changes

---
+++ modul10/parser.py
@@ -0,0 +1,135 @@
+from collections import UserDict
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = self.record
+
+
+class Field:
+    pass
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def del_phone(self, phone):
+        self.phones.remove(Phone(phone))
+
+    def change_phone(self, phone_old, phone_new):
+        self.phones[self.phones.index(Phone(phone_old))] = Phone(phone_new)
+
+
+class Name(Field):
+    def __init__(self, name):
+        self.value = name.capitalize()
+
+
+class Phone(Field):
+    def __init__(self, phone):
+        self.phone = phone
+
+
+kontakt_number = {}
+
+
+def hello():# функція привітання
+    text_output = "How can I help you?"
+    return text_output
+
+
+def add_phone(name_phone: list):# функція додає номер телефону
+    number_phone = Phone(int(name_phone[1]))
+    name = Name(name_phone[0])
+    self.data[record.name.value] = self.record
+    kontakt_number[name] = number_phone
+    text_output = "Number added"
+    return text_output
+
+
+def change(name_phone: list):# функція змінює номер телефону
+    number_phone = int(name_phone[1])
+    name = name_phone[0].capitalize()
+    kontakt_number[name]  # перевірка існування ім'я, штучна помилка
+    for char in filter(lambda value: value == name, kontakt_number.keys()):
+        kontakt_number[name] = number_phone
+    text_output = "Numer changed"
+    return text_output
+
+
+def phone(text: str):# функція видає номер телефону за імям
+    name = text[0].capitalize()
+    kontakt_number[name]  # перевірка існування ім'я, штучна помилка
+    return kontakt_number[name]
+
+
+def show_all(text: list):# функція видає весь список телефонів
+    if text[0] == 'all' and len(text) == 1:
+        return kontakt_number
+    # штучна помилка
+    raise KeyError
+
+
+# функції прощавання)
+def good_bye(text: list):
+    if text[0] == 'bye':
+        exit("Good bye")
+    # штучна помилка
+    raise KeyError
+
+
+def bye():
+    exit("Good bye")
+
+# словник функцій
+INPUT_HANDLER = {
+    "hello": hello,
+    "add": add_phone,
+    "change": change,
+    "del_phone": phone,
+    "show": show_all,
+    "good": good_bye,
+    "close": bye,
+    "exit": bye
+}
+
+
+def input_error(func):
+    def wrapper(text):
+        try:
+            return func(text)
+        except IndexError:
+            result = "Give me name and phone please splitted by space"
+        except KeyError:
+            result = "Enter right user's name or user's number or command"
+        except ValueError:
+            result = "Enter: add (name phone), change (name phone), \
+phone (name), show all, good bye, close, exit to continue"
+        except TypeError:
+            result = "Enter: add (name phone), change (name phone), \
+phone (name), show all, good bye, close, exit to continue"
+        return result
+    return wrapper
+
+
+@input_error
+def call_handler(text: str):
+    list_text = text.split(' ') #розбиваємо текст
+    if len(list_text) == 1:
+        return INPUT_HANDLER[list_text[0]]()#визиваємо функції без аргументів
+    else:
+        return INPUT_HANDLER[list_text[0]](list_text[1:])##визиваємо функції з аргументами
+
+
+def main():
+    while True:
+        text = input("Hello, input command: ").lower()
+        print(call_handler(text))
+
+
+if __name__ == "__main__":
+    main()
