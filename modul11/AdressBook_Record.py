e0d1b8361080c68377e8a4136c77b15514413e56
olhaishchenko <olga.ua.olga@gmail.com>
2022-12-03 20:00:56 +0100

    commit changes

--- modul11/AdressBook_Record.py
+++ modul11/AdressBook_Record.py
@@ -22,7 +22,17 @@ class AddressBook(UserDict):
 
 
 class Field:
-    pass
+    def __init__(self, value):
+        self._value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
 
 
 class Record:
@@ -45,9 +55,16 @@ class Record:
         self.birthday = Birthday(b_day)
 
     def days_to_birthday(self):
+        if not self.birthday:
+            raise ValueError('Day of birthday not exists')
         day_today = datetime.today().date()
-
-        self.birthday
+        next_b_day = datetime(self.birthday).date()
+        # if day_today.month > next_b_day.month and day_today.day > next_b_day.day:
+        if day_today > next_b_day:
+            next_b_day.year = day_today.year + 1
+        else:
+            next_b_day.year = day_today.year
+        return (next_b_day-day_today).days
 
     def __str__(self):
         return f'{self.name}: {", ".join([str(phone) for phone in self.phones])}, {self.birthday}'
@@ -58,6 +75,7 @@ class Record:
 
 class Name(Field):
     def __init__(self, name):
+        self._value = None
         self.value = name
 
     def __str__(self):
@@ -69,21 +87,44 @@ class Name(Field):
 
 class Phone(Field):
     def __init__(self, phone):
-        self.phone = phone
+        self._value = None
+        self.value = phone
+
+    @Field.value.setter
+    def value(self, value):
+        if not value.isnumeric():
+            raise ValueError('Wrong phones.')
+        if len(value) != 12:
+            raise ValueError("Phone must contains 12 symbols.")
+        self._value = value
 
     def __str__(self):
-        return self.phone
+        return self.value
 
     def __repr__(self):
-        return self.phone
+        return self.value
 
     def __eq__(self, other):
-        return self.phone == other.phone
+        return self.value == other.value
 
 
 class Birthday(Field):
     def __init__(self, birthday):
+        self._value = None
         self.value = birthday
+
+    @Field.value.setter
+    def value(self, value):
+        day_today = datetime.today().date()
+        b_day = datetime.strptime(str(value), "%d.%m.%Y").date()
+        if b_day > day_today:
+            raise ValueError("Birthday can't be more than current year and date.")
+        self._value = value
+
+    # def __gt__(self, other):
+    #     first_day = datetime.strptime(str(self.value), "%d.%m.%Y").date()
+    #     second_day = datetime.strptime(str(other), "%d.%m.%Y").date()
+    #     return first_day.month > second_day.month and first_day.day > second_day.day
 
     def __str__(self):
         return self.value
