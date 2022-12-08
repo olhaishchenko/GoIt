02e48e877b506e5f61312b8cb1834312d15b4885
olhaishchenko <olga.ua.olga@gmail.com>
2022-12-08 20:57:41 +0100

    commit changes

---
+++ modul12/AdressBook_Record.py
@@ -0,0 +1,147 @@
+import pickle
+from collections import UserDict
+from datetime import datetime
+
+
+class AddressBook(UserDict):
+    # адресна книга з контактами
+    # self.load_contacts_file()
+    def add_record(self, name):
+        self.data[name] = Record(name)
+
+    def exist(self, name):
+        return name in self.data
+
+    def check_record(self, name):
+        if not self.exist:
+            raise ValueError(f"Contact {name} not exists. Create contact")
+
+    def iterator(self, count=3):
+        records = []
+        i = 0
+        for record in self.data.values():
+            records.append(record)
+            i += 1
+            if i == count:
+                yield records
+                records = []
+                i = 0
+        if records:
+            yield records
+
+    # def save_contacts_file(self):
+    #     with open("address_book_contacts", "wb") as fh:
+    #         pickle.dump(self, fh)
+    #
+    # def load_contacts_file(self):
+    #     try:
+    #         with open("address_book_contacts", "rb") as fh:
+    #             self.data = pickle.load(fh)
+    #     except FileNotFoundError:
+    #         f"File not exists"
+    #     return self.data
+
+    def __str__(self):
+        return f'{self.data}'
+
+    def __repr__(self):
+        return f'{self.data}'
+
+
+class Field:
+    def __init__(self, value):
+        self._value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+    def __str__(self):
+        return f'{self.value}'
+
+    def __repr__(self):
+        return f'{self.value}'
+
+
+class Record:
+    # клас запис: видаляє, додає, змінює контакт
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+
+    def add_phone(self, phone_new):
+        self.phones.append(Phone(phone_new))
+
+    def change_phone(self, phone_old, phone_new):
+        self.phones[self.phones.index(Phone(phone_old))] = Phone(phone_new)
+
+    def del_phone(self, phone_old):
+        self.phones.remove(Phone(phone_old))
+
+    def add_birthday(self, b_day):
+        self.birthday = Birthday(b_day)
+
+    def days_to_birthday(self):
+        return self.birthday.days_to_birthday()
+
+    def __str__(self):
+        return f'{self.name}: {", ".join([str(phone) for phone in self.phones])}, {self.birthday}'
+
+    def __repr__(self):
+        return f'{self.name}: {", ".join([str(phone) for phone in self.phones])}, {self.birthday}'
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+
+    @Field.value.setter
+    def value(self, value):
+        if not value.isnumeric():
+            raise ValueError('Wrong phones.')
+        if len(value) != 12:
+            raise ValueError("Phone must contains 12 symbols.")
+        self._value = value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+
+class Birthday(Field):
+    @Field.value.setter
+    def value(self, value):
+        day_today = datetime.today()
+        try:
+            b_day = datetime.strptime(value, "%d.%m.%Y")
+        except ValueError:
+            raise ValueError("Incorrect date format, should be `DD.MM.YYYY`")
+        if b_day.date() > day_today.date():
+            raise ValueError("Birthday can't be more than current year and date.")
+        self._value = b_day
+
+    def days_to_birthday(self):
+        if not self._value:
+            raise ValueError('Day of birthday not exists')
+        day_today = datetime.today().date()
+        next_b_day = self._value.date()
+        if day_today.month > next_b_day.month:
+            next_b_day = next_b_day.replace(year=day_today.year + 1)
+        elif day_today.month == next_b_day.month and day_today.day > next_b_day.day:
+            next_b_day = next_b_day.replace(year=day_today.year + 1)
+        else:
+            next_b_day = next_b_day.replace(year=day_today.year)
+        return (next_b_day-day_today).days
+
+    def __str__(self):
+        return datetime.strftime(self.value, "%d.%m.%Y")
+
+    def __repr__(self):
+        return datetime.strftime(self.value, "%d.%m.%Y")
---
+++ modul12/parser111.py
@@ -0,0 +1,196 @@
+# Додамо поле для дня народження Birthday. Це поле не обов'язкове, але може бути тільки одне.
+# Додамо функціонал роботи з Birthday у клас Record, а саме функцію days_to_birthday,
+# яка повертає кількість днів до наступного дня народження.
+# Додамо функціонал перевірки на правильність наведених значень для полів Phone, Birthday.
+
+# Додамо пагінацію (посторінковий висновок) для AddressBook для ситуацій,
+# коли книга дуже велика і треба показати вміст частинами, а не все одразу.
+# Реалізуємо це через створення ітератора за записами.
+#
+# AddressBook реалізує метод iterator, який повертає генератор за записами AddressBook
+# і за одну ітерацію повертає уявлення для N записів.
+# Клас Record приймає ще один додатковий (опціональний) аргумент класу Birthday
+# Клас Record реалізує метод days_to_birthday, який повертає кількість днів до
+# наступного дня народження контакту, якщо день народження заданий.
+# setter та getter логіку для атрибутів value спадкоємців Field.
+# Перевірку на коректність веденого номера телефону setter для value класу Phone.
+# Перевірку на коректність веденого дня народження setter для value класу Birthday.
+import datetime
+
+from AdressBook_Record import AddressBook, Record
+
+address_book = AddressBook()
+
+
+def hello():  # функція привітання
+    return f"How can I help you?\n\
+Enter: \n'create name' (створюємо контакт)\n\
+'add name phones' (додаємо ім'я та номери телефонів)\n\
+'birthday' (додаємо ім'я день народження)\n\
+'next_birthday'(рахуємо дні до дня народження за ім'ям)\n\
+'change name phone_old phone_new' (міняє старий номер телефона на новий)\n\
+'del name phone' (видаляє номер телефону)\n\
+'phone name' (видає номер телефону за іменем)\n\
+'show' (показує всі контакти) to continue;\n\
+'close or exit' to exit"
+
+
+def create_new_contact(text):  # створюємо контакт
+    name, phones = create_data(text)
+    address_book.add_record(name)
+    return f"created contact {name}"
+
+
+def add_phone_func(text):  # функція додає номер або номери телефонів
+    name, phones = create_data(text)
+    address_book.check_record(name)
+    if not phones:
+        return f"No numbers added"
+    for phone in phones:
+        address_book[name].add_phone(phone)
+    return f"Number added"
+
+
+def add_birthday(text): # функція додає день народження
+    name, day = create_data(text)
+    bday = str(day[-1])
+    address_book[name].add_birthday(bday)
+    return f"birthday contact {name} added"
+
+
+def day_to_birt_func(text): # функція рахує дні до дня народження
+    name, day = create_data(text)
+    days = address_book[name].days_to_birthday()
+    return days
+
+
+def change_phone_func(text):  # функція змінює номер телефону
+    name, phones = create_data(text)
+    if len(phones) < 2:
+        raise IndexError(f"Please provide two phone numbers")
+    number_phone_old = phones[0]
+    number_phone_new = phones[1]
+    address_book.check_record(name)
+    address_book[name].change_phone(number_phone_old, number_phone_new)
+    return f"Number changed"
+
+
+def get_phone(text: str):  # функція видає номер телефону за імям
+    name, phones = create_data(text)
+    address_book.check_record(name)
+    return address_book[name]
+
+
+def del_phone(text):
+    name, phones = create_data(text)
+    address_book.check_record(name)
+    if not phones:
+        raise KeyError
+    for phone in phones:
+        address_book[name].del_phone(phone)
+    return f"Number deleted"
+
+
+def show_all():  # функція видає весь список телефонів
+    records = ''
+    for record in address_book.iterator():
+        records += str(record) + '\n'
+    return records
+
+
+# функції прощавання
+def bye():
+    exit("Good bye")
+
+
+# словник функцій
+INPUT_HANDLER = {
+    "add": add_phone_func,
+    "birthday": add_birthday,
+    "close": bye,
+    "change": change_phone_func,
+    "create": create_new_contact,
+    "del": del_phone,
+    "exit": bye,
+    "hello": hello,
+    "next_birthday": day_to_birt_func,
+    "phone": get_phone,
+    "show": show_all
+}
+
+
+def input_error(func):
+    def wrapper(text):
+        try:
+            return func(text)
+        except IndexError as e:
+            result = "Give me name and phone please splitted by space.\n" + str(e)
+        except KeyError as e:
+            result = "Enter right user's name or user's number or command.\n" + str(e)
+        except ValueError as e:
+            result = "Enter: add (name phone), change (name phone), \
+phone (name), show all to continue; good bye, close, exit to exit.\n" + str(e)
+        except TypeError as e:
+            result = "Enter: add (name phone), change (name phone), \
+phone (name), show all, good bye, close, exit to continue.\n" + str(e)
+        return result
+
+    return wrapper
+
+
+@input_error
+def call_handler(text: str):
+    new_text = text
+    list_text = ''
+    for key in INPUT_HANDLER:
+        if text.strip().lower().startswith(key):
+            new_text = key
+            list_text = text[len(key):]
+            list_text = list_text.strip()
+            break
+    if list_text:
+        return INPUT_HANDLER[new_text](list_text)  # визиваємо функції з аргументами
+    else:
+        return INPUT_HANDLER[new_text]()  # визиваємо функції без аргументів
+
+
+def create_data(text):
+    """
+    Розділяє вхідні дані на дві частини - номер і телефон.
+    Також ці данні проходять валідацію.
+    Для подальшої роботи з ними.
+    :param text: Строка з номером і ім'ям.
+    :return: Вже розділені ім'я і номер
+    """
+    name, *phones = text.strip().split(' ')
+    return name, phones
+
+
+address_book.add_record("olga")
+address_book.add_record("olga1")
+address_book.add_record("olga2")
+address_book.add_record("olga3")
+address_book.add_record("olga4")
+address_book.add_record("olga5")
+address_book.add_record("olga6")
+address_book.add_record("olga7")
+address_book.add_record("olga8")
+address_book.add_record("olga9")
+address_book.add_record("olga10")
+
+address_book["olga"].add_phone("242222000000")
+address_book["olga"].add_birthday('05.04.1980')
+
+
+def main():
+    print(hello())
+    try:
+        while True:
+            text = input("Input command: ")
+            print(call_handler(text))
+    finally:
+        address_book.save_contacts_file()
+
+
+if __name__ == "__main__":
+    main()
