8e337a07cd3c5d91165db0722e6642eeff269396
olhaishchenko <olga.ua.olga@gmail.com>
2023-02-11 22:17:12 +0100

    modul5

--- Python_web/modul5/main.py
+++ Python_web/modul5/main.py
@@ -1,3 +1,4 @@
+import json
 import platform
 import sys
 
@@ -6,20 +7,24 @@ import asyncio
 
 from datetime import timedelta, datetime
 
-if len(sys.argv) > 1:
-    count_date = int(sys.argv[1])
-today = datetime.today().date()
-date = today.strftime("%d.%m.%Y")
-data = []
-data.append(date)
-while count_date > 1:
-    day = timedelta(days=1)
-    begin_interval = today - day
-    data.append(begin_interval.strftime("%d.%m.%Y"))
-    today=begin_interval
-    count_date -=1
-print(data)
-async def main():
+
+def list_days(count: int) -> list:
+    if count >= 10:
+        raise ValueError("Among days can't be more 10")
+        count = 10
+    today = datetime.today().date()
+    date = today.strftime("%d.%m.%Y")
+    data = [date]
+    while count > 1:
+        day = timedelta(days=1)
+        new_day = today - day
+        data.append(new_day.strftime("%d.%m.%Y"))
+        today = new_day
+        count -= 1
+    return data
+
+
+async def p24call(date) -> dict:
 
     async with aiohttp.ClientSession() as session:
         async with session.get('https://api.privatbank.ua/p24api/exchange_rates?json&date=' + date) as response:
@@ -31,13 +36,27 @@ async def main():
             return result
 
 
-if __name__ == "__main__":
+def get_exchange_rates(days) -> list[dict]:
+
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main())
-    exchange_rate = {r['date']: {'EUR': {}, 'USD': {}}}
-    for rate in r['exchangeRate']:
-        if rate.get('currency') in ['EUR', 'USD']:
-            exchange_rate[r['date']][rate['currency']]['sale']=rate['saleRate']
-            exchange_rate[r['date']][rate['currency']]['purchase'] = rate['purchaseRate']
-    print(exchange_rate)
+    answer = []
+    for day in days:
+        r = asyncio.run(p24call(day))
+        exchange_rate = {r['date']: {'EUR': {}, 'USD': {}}}
+        for rate in r['exchangeRate']:
+            if rate.get('currency') in ['EUR', 'USD']:
+                exchange_rate[r['date']][rate['currency']]['sale']=rate['saleRate']
+                exchange_rate[r['date']][rate['currency']]['purchase'] = rate['purchaseRate']
+        print (exchange_rate)
+        answer.append(exchange_rate)
+    return answer
+
+
+if __name__ == "__main__":
+    count_date = 1
+    if len(sys.argv) > 1:
+        count_date = int(sys.argv[1])
+    result = get_exchange_rates(list_days(count_date))
+    print(json.dumps(result, indent=2, ensure_ascii=False))
+
