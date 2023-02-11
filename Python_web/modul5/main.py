9d8d4d416efb6a8c71fd07cf0e8c00300e9ed685
olhaishchenko <olga.ua.olga@gmail.com>
2023-02-11 18:37:31 +0100

    modul5

--- Python_web/modul5/main.py
+++ Python_web/modul5/main.py
@@ -4,16 +4,22 @@ import sys
 import aiohttp
 import asyncio
 
-from datetime import date
+from datetime import timedelta, datetime
 
 if len(sys.argv) > 1:
     count_date = sys.argv[1]
-data = date.today().strftime("%d.%m.%Y")
-
+today = datetime.today().date()
+date = today.strftime("%d.%m.%Y")
+data = []
+data.append(date)
+while count_date > 1:
+    day = timedelta(days=1)
+    begin_interval = today - day
+    data.append(begin_interval.strftime("%d.%m.%Y"))
 async def main():
 
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://api.privatbank.ua/p24api/exchange_rates?json&date='+ data) as response:
+        async with session.get('https://api.privatbank.ua/p24api/exchange_rates?json&date=' + date) as response:
             # print("Status:", response.status)
             # print("Content-type:", response.headers['content-type'])
             # print('Cookies: ', response.cookies)
@@ -32,4 +38,3 @@ if __name__ == "__main__":
             exchange_rate[r['date']][rate['currency']]['sale']=rate['saleRate']
             exchange_rate[r['date']][rate['currency']]['purchase'] = rate['purchaseRate']
     print(exchange_rate)
-    print(date.today().strftime("%d.%m.%Y"))
