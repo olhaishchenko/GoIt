c21bab3815fbd30ded41578a83b4ad9b2a61f204
olhaishchenko <olga.ua.olga@gmail.com>
2023-04-19 20:45:35 +0200

    example modul 13

---
+++ Python_web/modul13/main.py
@@ -0,0 +1,56 @@
+import configparser
+from pathlib import Path
+
+import uvicorn
+from fastapi import FastAPI, BackgroundTasks
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
+from pydantic import EmailStr, BaseModel
+from typing import List
+
+file_config = Path(__file__).parent.joinpath('conf/config.ini')
+config = configparser.ConfigParser()
+config.read(file_config)
+
+username = config.get('MAIL_MN', 'USER')
+password = config.get('MAIL_MN', 'PASSWORD')
+
+
+class EmailSchema(BaseModel):
+    email: EmailStr
+
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=username,
+    MAIL_PASSWORD=password,
+    MAIL_FROM=username,
+    MAIL_PORT=465,
+    MAIL_SERVER="smtp.meta.ua",
+    MAIL_FROM_NAME="Example email",
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
+)
+
+app = FastAPI()
+
+
+@app.post("/send-email")
+async def send_in_background(background_tasks: BackgroundTasks, body: EmailSchema):
+    message = MessageSchema(
+        subject="Fastapi mail module",
+        recipients=[body.email],
+        template_body={"fullname": "Billy Jones"},
+        subtype=MessageType.html
+    )
+
+    fm = FastMail(conf)
+
+    background_tasks.add_task(fm.send_message, message, template_name="example_email.html")
+
+    return {"message": "email has been sent"}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', port=8000, reload=True)
